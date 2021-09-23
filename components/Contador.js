d43b882a919d958f576ecbeb085cd771ff8555e1
Alan Gómez <73570083+ZeroxNeko@users.noreply.github.com>
2021-09-22 19:01:03 -0500

    Add files via upload

---
+++ components/Contador.js
@@ -0,0 +1,19 @@
+//Definiendo nuestro componente contador
+const Contador = () => {
+    //Definiendo el estado del componente. Cada vez que el estado cambia todo el componente se volverá a renderizar.
+    const [contador, setContador] = React.useState(0);
+
+    //Definiendo las funciones para aumentar y disminuirel estado del contador ambas invocan a la función setContador.
+    const aumentar = () => setContador(contador + 1);
+    const disminuir = () => setContador(contador -1);
+
+    return (
+        <div>
+            <h2 className={ contador < 0 ? "mayor" : "menor"}>Contador: {contador}</h2>
+            <hr />
+            <button onClick={aumentar}>Aumentar</button>
+            <button onClick={disminuir}>Disminuir</button>
+        </div>
+    );
+}
+
